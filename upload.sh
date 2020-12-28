ae57bcda1189d0762aaae164577fe478528069d4
Lassi Kortela <lassi@lassi.io>
2020-12-28 12:45:21 +0200

    Initial commit

---
+++ upload.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+mkdir -p srfi/support
+curl --location --fail --silent --show-error -o srfi/support/index.html \
+    https://raw.githubusercontent.com/schemedoc/srfi-metadata/master/table.html
+curl --location --fail --silent --show-error -o style.css \
+    https://www.staging.scheme.org/style.css
+rsync -vax index.html srfi style.css \
+    alpha.servers.scheme.org:/production/doc/www/

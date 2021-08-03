8b0a0db806ae494b043cc3ea5c30559a30723184
Lassi Kortela <lassi@lassi.io>
2021-08-03 13:21:09 +0300

    Rename style.css -> schemeorg.css

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 mkdir -p srfi/support
 curl --location --fail --silent --show-error -o srfi/support/index.html \
     https://raw.githubusercontent.com/schemedoc/srfi-metadata/master/table.html
-curl --location --fail --silent --show-error -o style.css \
-    https://www.staging.scheme.org/style.css
-rsync -crv index.html srfi style.css \
+curl --location --fail --silent --show-error -o schemeorg.css \
+    https://www.staging.scheme.org/schemeorg.css
+rsync -crv index.html srfi schemeorg.css \
     alpha.servers.scheme.org:/production/doc/www/

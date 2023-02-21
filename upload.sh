81e22c3351a9a31ecfa89576f66e50362b47a327
Lassi Kortela <lassi@lassi.io>
2023-02-21 16:35:57 +0200

    Use production stylesheet

--- upload.sh
+++ upload.sh
@@ -5,5 +5,5 @@ mkdir -p www/srfi/support
 curl --location --fail --silent --show-error -o www/srfi/support/index.html \
     https://raw.githubusercontent.com/schemedoc/srfi-metadata/master/table.html
 curl --location --fail --silent --show-error -o www/schemeorg.css \
-    https://www.staging.scheme.org/schemeorg.css
+    https://www.scheme.org/schemeorg.css
 rsync -crv www/ alpha.servers.scheme.org:/production/docs/www/

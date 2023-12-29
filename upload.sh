887606438b3e4be4a46274c012df5fcffccd85ba
Lassi Kortela <lassi@lassi.io>
2023-12-29 16:11:31 +0200

    Update upload script

--- upload.sh
+++ upload.sh
@@ -6,4 +6,4 @@ curl --location --fail --silent --show-error -o www/srfi/support/index.html \
     https://raw.githubusercontent.com/schemedoc/srfi-metadata/master/table.html
 curl --location --fail --silent --show-error -o www/schemeorg.css \
     https://www.scheme.org/schemeorg.css
-rsync -crv www/ alpha.servers.scheme.org:/production/docs/www/
+rsync -crv www/ tuonela.scheme.org:/production/docs/www/
